a875b80b33afe9b5ac552ffb18be5c9a12fd1d71
dadlo <dadlobr@gmail.com>
2015-05-15 21:42:30 -0300

    adiciona gettext to footer

--- cms2/templates/footer.php
+++ cms2/templates/footer.php
@@ -1,19 +1,19 @@
       <div class="jumbotron footer" id="arodape">
           <div class="container">
               <div class="col-md-8" id="lastest-news" >
-                <h2>Área de Gestão QvQ</h2>
+                <h2><?php echo _( 'Área de Gestão QvQ'); ?> </h2>
                 <div class="listanoticia">
                   <dl class="dl-horizontal">
-                    <p>Registre-se enviando seus dados para:</p>
+                    <p><?php echo _( 'Registre-se enviando seus dados para:'); ?> </p>
 		     <p><a href="mailto:cadastro@quizvsquiz.com.br">cadastro@quizvsquiz.com.br</a></p>
 		  </dl>
                 </div>
               </div>
               <div class="col-md-4">
-                  <h3>QvQ - Quiz Vs Quiz</h3><br>
-                  Projeto desenvolvido como<br>
-                  trabalho de conclusão de curso de<br>
-                  Tecnologia em Jogos Digitais da<br>
+                  <h3><?php echo _( 'QvQ - Quiz Vs Quiz'); ?> </h3><br>
+                  <?php echo _( 'Projeto desenvolvido como'); ?> <br>
+                  <?php echo _( 'trabalho de conclusão de curso de'); ?> <br>
+                  <?php echo _( 'Tecnologia em Jogos Digitais da'); ?> <br>
                   FATEC - São Caetano do Sul
               </div>
           </div>
